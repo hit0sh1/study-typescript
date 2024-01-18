c2f02094710e6f39ac55ca927e09d6b9c320481e
hit0sh1 <h.miyagawa365@gmail.com>
2024-01-18 23:46:44 +0900

    ✨ Todo追加機能実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import type { NextPage } from "next";
-import { useState } from "react";
+import { Input } from "postcss";
+import { ChangeEventHandler, useState } from "react";
+import { text } from "stream/consumers";
+
+type Todo = { id: number; label: string; isDone: boolean };
 
 const Home: NextPage = () => {
-    const [todos, setTodos] = useState([
-        { id: Math.random(), label: "TODO1", isDone: false },
-        { id: Math.random(), label: "TODO2", isDone: true },
-        { id: Math.random(), label: "TODO3", isDone: false },
-        { id: Math.random(), label: "TODO4", isDone: false },
-    ]);
+    const [text, setText] = useState("");
+    const [todos, setTodos] = useState<Todo[]>([]);
 
-    const toggle = (e) => {
+    const input: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setText(e.target.value);
+    };
+
+    const add = () => {
+        setTodos((prevTodos) => {
+            return [...prevTodos, { id: Math.random(), label: text, isDone: false }];
+        });
+        setText("");
+    };
+
+    const toggle: ChangeEventHandler<HTMLInputElement> = (e) => {
         setTodos((prevTodos) => {
             return prevTodos.map((todo) => {
                 if (todo.id === Number(e.target.value)) {
@@ -26,20 +37,28 @@ const Home: NextPage = () => {
         <div className="w-96 mx-auto p-20">
             <h1 className="text-xl font-bold">Todo</h1>
             <div className="flex gap-x-2">
-                <input type="text" className="border border-white" />
-                <button className="border border-white flex-shrink-0 px-2">追加</button>
+                <input type="text" value={text} onChange={input} className="border border-white text-black" />
+                <button onClick={add} className="border border-white flex-shrink-0 px-2">
+                    追加
+                </button>
             </div>
             <ul className="mt-4 space-y-2">
                 {todos.map((todo) => (
                     <li key={todo.id}>
-                        <label className="flex items-center gap-x-2">
-                            <input type="checkbox" value={todo.id} checked={todo.isDone} onChange={toggle} />
-                            <span>{todo.label}</span>
-                        </label>
+                        <ListItem todo={todo} toggle={toggle} />
                     </li>
                 ))}
             </ul>
         </div>
+    );
+};
+
+const ListItem: VFC<{ todo: Todo; toggle: ChangeEventHandler<HTMLInputElement> }> = (todo, toggle) => {
+    return (
+        <label className="flex items-center gap-x-2">
+            <input type="checkbox" value={todo.id} checked={todo.isDone} onChange={toggle} />
+            <span>{todo.label}</span>
+        </label>
     );
 };
 
